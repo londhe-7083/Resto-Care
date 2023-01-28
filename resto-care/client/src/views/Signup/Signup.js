9885e1c88c6e8aef239921860fab727a2b40e551
londhe-7083 <shubhamlondhe1583@gmail.com>
2023-01-29 00:44:23 +0530

    signup page edited

--- resto-care/client/src/views/Signup/Signup.js
+++ resto-care/client/src/views/Signup/Signup.js
@@ -1,17 +1,17 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import "./Signup.css"
 
 
-function Signup(){
-  const [name, setName] =useState('')
-  const [email, setEmail] =useState('')
-  const [phone, setPhone] =useState('')
-  const [password, setPassword] =useState('')
-  const [role, setRole] =useState('user')
+function Signup() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState('user')
 
-  async function signupUser(){
-    const response =  await axios.post('/signup',{
+  async function signupUser() {
+    const response = await axios.post('/signup', {
       name: name,
       email: email,
       phone: phone,
@@ -20,66 +20,71 @@ function Signup(){
 
     })
     console.log(response.data)
-    if(response.data.success){
+    if (response.data.success) {
       alert(response.data.success)
-        alert(response.data.message)
-        window.location.href = '/login'
-      }
-      else{
-        alert(response.data.message)
-        setName('')
-        setEmail('')
-        setPhone('')
-        setPassword('')
-
-      }
+      alert(response.data.message)
+      window.location.href = '/login'
+    }
+    else {
+      alert(response.data.message)
+      setName('')
+      setEmail('')
+      setPhone('')
+      setPassword('')
 
     }
+
+  }
 
   return (
     <div>
       <div className='maindiv'>
-      <h1 className='text-center'>Signup</h1>
-      <div className='row'>
-        <div className=' col-6 image'>
-          <img src={require('./images/firstimg.jpg')} alt='' ></img>
-        </div>
+        <div className='row'>
+          <div className=' col-6 image'>
+            <img src={require('./images/firstimg.jpg')} alt='' ></img>
+          </div>
 
-        <div className='col-6' >
+
+          <div className='col-6' >
             <div className='form-containeer'>
-            <form>
-                  <div className='text'>keep connected with us please create your account🔔 </div>
-                <div  className='input-box'>
-                    <label htmlFor='name'>Full Name: </label>
-                    <input type='text' id='name'  className='user-input' value={name}
-                     onChange={(e) =>setName(e.target.value)}/>
-                </div>
-
-
-                <div  className='input-box'>
-                <i class="fa-thin fa-envelope"></i>
-                    <label htmlFor='email'>Email Address: </label>
-                    <input type='email' id='email' value=
-                    {email} className='user-input'  onChange={(e) =>setEmail(e.target.value)}/>
+              <div className='avtar'>
+                <img src={require('./images/avtar.png')} alt='' ></img>
+              </div>
+              <form>
+              <h1 className='text-center'>Welcome</h1>
+                <div className='input-box input-box-a'>
+                  <i class="fa-regular fa-user"></i>
+                  <label htmlFor='name'>Full Name: </label>
+                  <input type='text' id='name' className='user-input' value={name}
+                    onChange={(e) => setName(e.target.value)} />
                 </div>
 
                 <div className='input-box'>
-                    <label htmlFor='phone'> Phone: </label>
-                    <input type='phone' id='phone' className='user-input' value={phone} onChange={(e) =>setPhone(e.target.value)}/>
+                  <i class="fa-regular fa-envelope"></i>
+                  <label htmlFor='email'>Email Address: </label>
+                  <input type='email' id='email' value=
+                    {email} className='user-input' onChange={(e) => setEmail(e.target.value)} />
                 </div>
 
-                <div>
-                    <label htmlFor='password'>Password: </label>
-                    <input type='password' id='password' placeholder='Enter Password' className='user-input' value={password} onChange={(e) =>setPassword(e.target.value)}/>
+                <div className='input-box'>
+                  <i class="fa-solid fa-phone"></i>
+                  <label htmlFor='phone'> Phone: </label>
+                  <input type='phone' id='phone' className='user-input' value={phone} onChange={(e) => setPhone(e.target.value)} />
+                </div>
+
+                <div className='input-box '>
+                   <i class="fa-solid fa-lock"></i>
+                  <label htmlFor='password'>Password: </label>
+                  <input type='password' id='password' className='user-input' value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div>
                   <button type='button' className='signup-button' onClick={signupUser}> Signup </button>
                 </div>
-            </form>
+              </form>
             </div>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   )
 }
